2600b6c1618bf1f953747dfdc3da9520f04f26e8
Alexis Marquet <alexis@alexismarquet.com>
2022-10-21 15:23:33 +0200

    fix downloadint tarballs from synology server instead of sourceforge

--- release.sh
+++ release.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-VERSIONS=(6.2 7.0)
+VERSIONS=(7.1)
 ARCHS=(
-    "apollolake"
-    "armada38x"
-    "avoton"
     "braswell"
-    "broadwell"
-    "broadwellnk"
-    "bromolow"
-    "cedarview"
-    "denverton"
-    "geminilake"
-    "kvmx64"
-    "monaco"
-    "rtd1296"
-    "x64"
+    "v1000"
 )
 
 set -e
@@ -24,21 +12,22 @@ if [ `id -u` -ne 0 ]; then
     echo "This script must be run as root"
     exit 1
 fi
-
+mkdir -p toolkit_tarballs
 # Download all necessary tarballs before calling into the docker containers.
 echo "Downloading environment tarballs"
 for ver in ${VERSIONS[@]}; do
-    url_base="https://sourceforge.net/projects/dsgpl/files/toolkit/DSM$ver"
+    url_base="https://global.download.synology.com/download/ToolChain/toolkit/$ver"
     pushd toolkit_tarballs/
     if [ ! -f base_env-$ver.txz ]; then
-        wget -q --show-progress "$url_base/base_env-$ver.txz"
+        echo "$url_base/base_env-$ver.txz"
+        wget -q --show-progress "$url_base/base/base_env-$ver.txz"
     fi
     for arch in ${ARCHS[@]}; do
         if [ ! -f ds.$arch-$ver.dev.txz ]; then
-            wget -q --show-progress "$url_base/ds.$arch-$ver.dev.txz"
+            wget -q --show-progress "$url_base/$arch/ds.$arch-$ver.dev.txz"
         fi
         if [ ! -f ds.$arch-$ver.env.txz ]; then
-            wget -q --show-progress "$url_base/ds.$arch-$ver.env.txz"
+            wget -q --show-progress "$url_base/$arch/ds.$arch-$ver.env.txz"
         fi
     done
     popd
